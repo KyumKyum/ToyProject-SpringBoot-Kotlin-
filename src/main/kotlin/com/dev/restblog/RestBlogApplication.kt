3f3a57eac4318b7449bc309d83115f5a0dab07fc
KyumKyum <mycheesepasta@gmail.com>
2023-06-29 14:43:12 +0900

    dir

--- src/main/kotlin/com/dev/restblog/RestBlogApplication.kt
+++ src/main/kotlin/com/dev/restblog/RestBlogApplication.kt
@@ -1,7 +1,10 @@
 package com.dev.restblog
 
+import lombok.RequiredArgsConstructor
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @SpringBootApplication
+@RequiredArgsConstructor
 class RestBlogApplication {
-    @RequestMapping("/")
-    fun home() = "Test!"
+
+//    val mainRouter: MainRouter = MainRouter();
+//    @RequestMapping("/welcome")
+//    fun root(): MainRouter {
+//        return mainRouter
+//    }
+
 }
 
 fun main(args: Array<String>) { // Run Spring Application: Start spring -> auto-configured Tomcat web server.
